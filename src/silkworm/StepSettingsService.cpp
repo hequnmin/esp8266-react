1260b3e7ea72095a66f5c88fb5928f4bab914c1f
hequnmin <min@becheer.com>
2019-07-14 23:26:30 +0800

    gpio intr

--- src/silkworm/StepSettingsService.cpp
+++ src/silkworm/StepSettingsService.cpp
@@ -39,11 +39,35 @@ void StepSettingsService::writeToJsonObject(JsonObject& root) {
 void StepSettingsService::onConfigUpdated() {
   	_lastManaged = millis() - MANAGE_STEP_DELAY;
 
-	stepNext = 0;
-    handleStep();
+	_stepNext = 0;
+    handleStep(0);
 }
 
-void StepSettingsService::handleStep(void) {
+void ICACHE_RAM_ATTR StepSettingsService::handleStep(uint8_t arg) {
+
+	switch (arg)
+	{
+		case 0:
+			Serial.println("Step 0");
+
+			SWITCH_WR_INIT();
+			PIN_FUNC_SELECT( SWITCH_MUX, SWITCH_FUNC );
+			ETS_GPIO_INTR_DISABLE();
+			ETS_GPIO_INTR_ATTACH( &handleStep, 1 );
+
+			gpio_pin_intr_state_set( GPIO_ID_PIN( SWITCH_NUM ), GPIO_PIN_INTR_POSEDGE );
+
+			GPIO_REG_WRITE( GPIO_STATUS_W1TC_ADDRESS, BIT(SWITCH_NUM) );
+			ETS_GPIO_INTR_ENABLE();
+			break;
+		case 1:
+			ETS_GPIO_INTR_DISABLE();
+			Serial.println("Step 1");
+			break;
+		default:
+			break;
+	}
+
 
 
 
--- src/silkworm/StepSettingsService.h
+++ src/silkworm/StepSettingsService.h
@@ -2,21 +2,32 @@
 #define StepSettingsService_h
 
 #include <Arduino.h>
-// #include <ets_sys.h>
-// #include <osapi.h>
-// #include <user_interface.h>
-// #include <eagle_soc.h>
-// #include <gpio.h>
-// #include <PWM.h>
+#include <ets_sys.h>
+#include <osapi.h>
+#include <user_interface.h>
+#include <eagle_soc.h>
+#include <gpio.h>
+#include <PWM.h>
 
 #include <silkworm/Comm.h>
 #include <SettingsService.h>
 
-#define MANAGE_STEP_DELAY 10
-#define GPIO_START 14
+#define MANAGE_STEP_DELAY               10
+#define GPIO_START                      14
 
-#define STEP_SETTINGS_FILE "/config/stepSettings.json"
-#define STEP_SETTINGS_SERVICE_PATH "/rest/stepSettings"
+#define STEP_SETTINGS_FILE              "/config/stepSettings.json"
+#define STEP_SETTINGS_SERVICE_PATH      "/rest/stepSettings"
+
+#define SWITCH_NUM		                5    //D1
+#define SWITCH_FUNC		                FUNC_GPIO5
+#define SWITCH_MUX		                PERIPHS_IO_MUX_GPIO5_U
+
+#define SWITCH_RD_INIT()	            GPIO_DIS_OUTPUT(SWITCH_NUM)
+#define SWITCH_WR_INIT()	            GPIO_OUTPUT_SET(SWITCH_NUM,0)
+#define SWITCH_SET_HIGH()               GPIO_OUTPUT_SET(SWITCH_NUM,1)
+#define SWITCH_SET_LOW()                GPIO_OUTPUT_SET(SWITCH_NUM,0)
+#define SWITCH_ACTIVE                   ( GPIO_INPUT_GET(SWITCH_NUM) != 0 )
+
 
 class StepSettingsService : public AdminSettingsService {
     public:
@@ -25,25 +36,28 @@ class StepSettingsService : public AdminSettingsService {
         ~StepSettingsService();
 
         void loop();
+
+  
+
     protected:
         void readFromJsonObject(JsonObject& root);
         void writeToJsonObject(JsonObject& root);
-        void onConfigUpdated();        
+        void onConfigUpdated();
+   
     private:
+
+
         // for the mangement delay loop
         unsigned long _lastManaged;
 
         uint8_t _stepMode;
-
-        unsigned long lastLog;
-        static os_timer_t timerStay;
-        uint8_t stepCurrent;
-        uint8_t stepNext;
+        uint8_t _stepNext;
+        uint8_t _stepCurrent;  
 
         void manageStart();
         void manageStep();
 
-        void handleStep();
+        static void handleStep(uint8_t arg);
 };
 
 
